2eb603e3deb2c09f3d97d05f06af86a0372ea5a6
liamiam <liam@liamiam.com>
2019-09-24 14:23:11 -0700

    Vector3D add *=, /=, +=, -=, *, /, -a, Magnitude, Normalize, a+b, a-b

--- Vector3D.hpp
+++ Vector3D.hpp
@@ -1,6 +1,8 @@
 #ifndef VECTOR3D_HPP
 #define VECTOR3D_HPP
 
+#include <cmath>
+
 struct Vector3D {
     float x, y, z;
 
@@ -15,6 +17,64 @@ struct Vector3D {
     float& operator [](int i) {
         return ((&x)[i]);
     }
+
+    Vector3D& operator *=(float s) {
+        x *= s;
+        y *= s;
+        z *= s;
+        return (*this);
+    }
+
+    Vector3D& operator /=(float s) {
+        s = 1.00f / s;
+        x *= s;
+        y *= s;
+        z *= s;
+        return (*this);
+    }
+
+    Vector3D& operator +=(const Vector3D& v) {
+        x += v.x;
+        y += v.y;
+        z += v.z;
+        return (*this);
+    }
+
+    Vector3D& operator -=(const Vector3D& v) {
+        x -= v.x;
+        y -= v.y;
+        z -= v.z;
+        return (*this);
+    }
 };
+
+inline Vector3D operator *(const Vector3D& v, float s) {
+    return Vector3D(v.x * s, v.y * s, v.z * s);
+}
+
+inline Vector3D operator /(const Vector3D& v, float s) {
+    s = 1.00f / s;
+    return Vector3D(v.x * s, v.y * s, v.z * s);
+}
+
+inline Vector3D operator -(const Vector3D& v) {
+    return Vector3D(-v.x, -v.y, -v.z);
+}
+
+inline float Magnitude(const Vector3D& v) {
+    return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+inline Vector3D Normalize(const Vector3D& v) {
+    return v / Magnitude(v);
+}
+
+inline Vector3D operator +(const Vector3D& a, const Vector3D b) {
+    return Vector3D(a.x + b.x, a.y + b.y, a.z + b.z);
+}
+
+inline Vector3D operator -(const Vector3D& a, const Vector3D b) {
+    return Vector3D(a.x - b.x, a.y - b.y, a.z - b.z);
+}
 
 #endif
--- test/Vector3D_test.cpp
+++ test/Vector3D_test.cpp
@@ -1,11 +1,91 @@
 #include "../Vector3D.hpp"
 #include <gtest/gtest.h>
 
-TEST(Vector3D_test, TryVector3D) {
+TEST(Vector3D, TryVector3D) {
     Vector3D a(1.5f, 3.7f, 32.25f);
     ASSERT_EQ(1.5f, a.x);
     ASSERT_EQ(3.7f, a.y);
     ASSERT_EQ(32.25f, a.z);
+}
+
+TEST(Vector3D, ScalarMultiplication) {
+    Vector3D a(1.f, 1.f, 1.f);
+    a *= 2.f;
+    ASSERT_FLOAT_EQ(a.x, 2.f);
+    ASSERT_FLOAT_EQ(a.y, 2.f);
+    ASSERT_FLOAT_EQ(a.z, 2.f);
+}
+
+TEST(Vector3D, ScalarDivision) {
+    Vector3D a(1.f, 1.f, 1.f);
+    a /= 2.f;
+    ASSERT_FLOAT_EQ(a.x, .5f);
+    ASSERT_FLOAT_EQ(a.y, .5f);
+    ASSERT_FLOAT_EQ(a.z, .5f);
+}
+
+TEST(Vector3D, VectorAdditionPlusEquals) {
+    Vector3D a(1.f, 2.f, 3.f);
+    a += a;
+    ASSERT_FLOAT_EQ(a.x, 2.f);
+    ASSERT_FLOAT_EQ(a.y, 4.f);
+    ASSERT_FLOAT_EQ(a.z, 6.f);
+    
+}
+
+TEST(Vector3D, VectorAddition) {
+    Vector3D a(1.f, 2.f, 3.f);
+    Vector3D b = a + a;
+    ASSERT_FLOAT_EQ(b.x, 2.f);
+    ASSERT_FLOAT_EQ(b.y, 4.f);
+    ASSERT_FLOAT_EQ(b.z, 6.f);
+}
+
+TEST(Vector3D, ScalarMultiplicationStarEquals) {
+    Vector3D a(1.f, 2.f, 3.f);
+    a *= 2.f;
+    ASSERT_FLOAT_EQ(a.x, 2.f);
+    ASSERT_FLOAT_EQ(a.y, 4.f);
+    ASSERT_FLOAT_EQ(a.z, 6.f);
+}
+
+TEST(Vector3D, VectorSubtractionMinusEquals) {
+    Vector3D a(2.f, 3.f, 4.f);
+    a -= Vector3D(1.f, 1.f, 1.f);
+    ASSERT_FLOAT_EQ(a.x, 1.f);
+    ASSERT_FLOAT_EQ(a.y, 2.f);
+    ASSERT_FLOAT_EQ(a.z, 3.f);
+    
+}
+
+TEST(Vector3D, VectorSubtraction) {
+    Vector3D a(2.f, 3.f, 4.f);
+    Vector3D b = a - Vector3D(1.f, 1.f, 1.f);
+    ASSERT_FLOAT_EQ(b.x, 1.f);
+    ASSERT_FLOAT_EQ(b.y, 2.f);
+    ASSERT_FLOAT_EQ(b.z, 3.f);
+}
+
+TEST(Vector3D, ScalarDivisionSlashEquals) {
+    Vector3D a(2.f, 4.f, 6.f);
+    a /= 2.f;
+    ASSERT_FLOAT_EQ(a.x, 1.f);
+    ASSERT_FLOAT_EQ(a.y, 2.f);
+    ASSERT_FLOAT_EQ(a.z, 3.f);
+}
+
+TEST(Vector3D, VectorMagnitude) {
+    Vector3D a(1.f, 2.f, 3.f);
+    float b = Magnitude(a);
+    ASSERT_FLOAT_EQ(ceil(b), 4.f);
+}
+
+TEST(Vector3D, VectorNormalize) {
+    Vector3D a(1.f, 2.f, 3.f);
+    Vector3D b = Normalize(a);
+    ASSERT_FLOAT_EQ(b.x, 0.26726124f);
+    ASSERT_FLOAT_EQ(b.y, 0.53452248f);
+    ASSERT_FLOAT_EQ(b.z, 0.80178373f);
 }
 
 int main(int argc, char **argv)
