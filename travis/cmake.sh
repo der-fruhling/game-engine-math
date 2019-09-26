c6b9c10331dc7c91dec746d446abdbf5bea7cc47
liamiam <liam@liamiam.com>
2019-09-25 20:20:32 -0700

    Finally works!

--- travis/cmake.sh
+++ travis/cmake.sh
@@ -6,6 +6,7 @@ sudo make && \
 sudo cp *.a /usr/lib/
 cd "$TRAVIS_BUILD_DIR" || exit 1
 cmake . && \
-make && \
-test/Matrix3D_test && \
+make
+test/Matrix3D_test
 test/Vector3D_test
+./game_engine_math
